a866ee7a9c0cd587387f2b97469740739088e24d
vytska69 <vytautas.duchas@gmail.com>
2021-11-07 11:18:08 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
-#
+    #
 # Copyright (C) 2021 AospExtended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_pine.mk
+    $(LOCAL_DIR)/evolution_pine.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_pine-user \
-    aosp_pine-userdebug \
-    aosp_pine-eng
+    evolution_pine-user \
+    evolution_pine-userdebug \
+    evolution_pine-eng
