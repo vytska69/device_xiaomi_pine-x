24723c988720633b4e43dd6ba3e740ad8aef34a1
vytska69 <vytautas.duchas@gmail.com>
2021-11-07 11:24:01 +0200

    Update aosp_pine.mk

--- aosp_pine.mk
+++ aosp_pine.mk
@@ -9,18 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common AospExtended stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from pine device
 $(call inherit-product, device/xiaomi/pine/device.mk)
 
-# Bootanimation res
-TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pine
-PRODUCT_NAME := aosp_pine
+PRODUCT_NAME := evolution_pine_pine
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7A
